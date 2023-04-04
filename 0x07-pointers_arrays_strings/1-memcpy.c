5eb41dbb084636f79d96f539d42fbe092c2ab00f
Spy 175 <arnoldnortey@hotmail.com>
2023-04-03 21:18:16 -0700

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,7 +5,7 @@
  * @src: memory where is copied
  * @n: number of bytes
  *
- * Return: copied memory with a byted changed
+ * Return: copied memory with n byted changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -14,7 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (; r < i; r++)
 	{
-		dest[r]\ = src[r];
+		dest[r] = src[r];
 		n--;
 	}
 	return (dest);
